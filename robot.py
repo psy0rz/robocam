f1151cb4f22dd5f2c9e80074d2480df088f35429
Edwin Eefting <edwin@datux.nl>
2024-11-16 16:54:57 +0100

    bla

--- robot.py
+++ robot.py
@@ -2,35 +2,11 @@ import cv2
 import numpy as np
 
 import detector
-from util import draw_corner_lines, draw_target_cross, distance_between_points
+from selector import Selector
+from util import draw_corner_lines, draw_target_cross
 import colormapper
 
 # Callback function for mouse click event
-
-
-
-class Selector:
-    # can search for a new object close to x,y,color, and keep tracking the ID when it has found one
-    def __init__(self):
-        # search for object with these specs
-        self.search_point = None
-        self.search_color = None
-
-        self.current_point = (0, 0)
-
-    def reset(self):
-        self.current_point = None
-
-    def update(self, point, color_name):
-        if self.search_color is not None and color_name != self.search_color:
-            return
-
-        if self.current_point is None or (self.search_point is not None
-                                          and distance_between_points(point,
-                                                                      self.search_point) < distance_between_points(
-                    self.search_point,
-                    self.current_point)):
-            self.current_point = point
 
 
 selector = Selector()
